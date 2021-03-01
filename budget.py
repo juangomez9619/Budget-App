9544241b1e9bda1e0cc39563dea29372edbe3a27
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2021-02-28 19:59:53 -0500

    Constructor for Category class. Deposit method added.

--- budget.py
+++ budget.py
@@ -1,7 +1,17 @@
 class Category:
+    
 
+    def __init__(self, category): # Constructor
+        self.category = category
+        self.ledger = list()
+        
+    
+    def deposit(self, amount, description = None):
+        if description == None:
+            self.ledger.append({"amount": amount, "description": ""})
+        else:
+            self.ledger.append({"amount": amount, "description": description})
 
-
-
-
+    
 def create_spend_chart(categories):
+    pass
