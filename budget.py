f54aa645726a24f4ffd3fbaa64f59b2379cda7e3
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2021-03-02 22:51:08 -0500

    Withdraw, get_balance and sum_ledger methods added and tested. Run 6 tests ok# Please enter the commit message for your changes. Lines starting

--- budget.py
+++ budget.py
@@ -12,6 +12,47 @@ class Category:
         else:
             self.ledger.append({"amount": amount, "description": description})
 
-    
+
+    def withdraw(self, amount, description = None):
+        sum_ledger = self.sum_ledger()
+        if sum_ledger <=0: return False
+        if description == None: self.ledger.append({"amount": -amount, "description": ""})
+        else: self.ledger.append({"amount": -amount, "description": description})
+        return True
+
+
+    def get_balance(self): return self.sum_ledger()
+    def sum_ledger(self):
+        '''
+        Returns the actual sum of the ladger. 
+        '''
+        ledger = self.ledger
+        ledger_values = [ledger[value]['amount'] for value in range(len(ledger))]
+        
+        return sum(ledger_values)
+
+    def transfer(self, amount, destination):
+        sum_ledger = self.sum_ledger()
+        if sum_ledger <=0: return False
+        self.withdraw(amount, f"Transfer to {destination.category}")
+        destination.deposit(amount, f"Transfer from {self.category}")
+        return True
+
+
+
+
+
+
+        
+
+
+
+        
+
+
+
+
+
+
 def create_spend_chart(categories):
     pass
