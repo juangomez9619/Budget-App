59de8dd7b9c7373233a51a77e0fda043e05344ce
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2021-03-15 21:07:27 -0500

    Final solution

--- budget.py
+++ budget.py
@@ -24,6 +24,11 @@ class Category:
         ledger = self.ledger
         return sum([ledger[value]['amount'] for value in range(len(ledger))])
 
+    def sum_withdraws(self):
+        ledger = self.ledger
+        return sum([-ledger[value]['amount'] for value in range(len(ledger)) if ledger[value]['amount'] < 0])
+    
+
 
     def transfer(self, amount, destination):
         if  not (self.check_funds(amount)): return False
@@ -68,8 +73,67 @@ class Category:
         return output
 
 def create_spend_chart(categories):
-    bar_chart = list()
-    bar_chart_values = [100]
-    for i in range 
 
-    return "Percentage spent by category"+"\n"
+    '''Getting name and sum of withdraws of each item in categories list'''
+    name_totalWithdraws = dict()
+    for category in categories: name_totalWithdraws[category.category] = category.sum_withdraws()
+
+    total = 0
+    for key in name_totalWithdraws:
+        total += name_totalWithdraws[key]
+    
+    bars = []
+    for key in name_totalWithdraws:
+        bars.append(compute_bar_height(name_totalWithdraws[key] / total * 100))
+    
+    bar_chart_values = ['100|',' 90|', ' 80|', ' 70|', ' 60|', ' 50|', ' 40|', ' 30|', ' 20|', ' 10|', '  0|']
+    dashes = ' '*(len(bar_chart_values[0])) 
+    output = "Percentage spent by category"+"\n"
+    
+    for i in range(len(bar_chart_values)):
+        for j in range(len(bars)):
+            bar_chart_values[i] += ' '+ bars[j][i]+' '
+        output += bar_chart_values[i]+' \n'
+
+    output += dashes+'---'*len(bars)+'-\n'
+
+    max_length = None
+    for key in name_totalWithdraws:
+        if max_length == None: max_length = len(key)
+        if len(key) >= max_length: max_length = len(key)
+    
+    category_names = []
+    for key in name_totalWithdraws:
+        category_names.append(name_splitter(key,max_length))
+    
+    final_print =''
+    for i in range(max_length):
+        final_print +='    '
+        for j in range(len(bars)):
+            final_print +=' ' + category_names[j][i] + ' '
+
+        if i< max(range(max_length)): final_print +=' \n'
+        else: final_print +=' '
+        
+    return output + final_print
+
+def compute_bar_height(value):
+    bar_height = 0
+    while value >= 10:
+        value -= 10
+        bar_height += 1
+    if value >= 5: bar_height += 1
+
+    bar = ["o"," "," "," "," "," "," "," "," "," ", " "]
+    for i in range(bar_height): bar[i+1]= "o"
+    return bar[::-1]
+
+def name_splitter(name, max_length):
+    output = []
+    for letter in range(max_length):
+        try:
+            output.append(name[letter])
+        except:
+            output.append(' ')
+    return output
+    
